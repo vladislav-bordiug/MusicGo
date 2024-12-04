ba0fb94004dcfa8349cb293f7e1899657353ccbe
Vladislav <vladislav.bordioug@gmail.com>
2024-12-04 13:59:59 +0300

    final

--- db.go
+++ db.go
@@ -24,30 +24,59 @@ func NewPGXDatabase(pool DBPool) *PGXDatabase {
 }
 
 func (db *PGXDatabase) CreateTableQuery(ctx context.Context) error {
-	_, err := db.pool.Exec(ctx, "CREATE TABLE IF NOT EXISTS songs (group_name TEXT, song_name TEXT, releaseDate TEXT, text TEXT, link TEXT);")
+	_, err := db.pool.Exec(ctx, "DROP TABLE IF EXISTS groups CASCADE;")
+	_, err = db.pool.Exec(ctx, "DROP TABLE IF EXISTS songs CASCADE;")
+	_, err = db.pool.Exec(ctx, "CREATE TABLE IF NOT EXISTS groups (id SERIAL PRIMARY KEY, group_name TEXT, CONSTRAINT unique_group UNIQUE(group_name));")
+	if err != nil {
+		return err
+	}
+	_, err = db.pool.Exec(ctx, "CREATE TABLE IF NOT EXISTS songs (id SERIAL PRIMARY KEY, song_name TEXT, releaseDate TIMESTAMP, text TEXT, link TEXT, group_id INTEGER, FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE, CONSTRAINT unique_group_song UNIQUE(group_id, song_name));")
 	return err
 }
 
 func (db *PGXDatabase) InsertQuery(ctx context.Context, group_name string, song_name string, releaseDate string, text string, link string) error {
-	_, err := db.pool.Exec(ctx, "INSERT INTO songs(group_name, song_name, releaseDate, text, link) values($1, $2, $3, $4, $5)", group_name, song_name, releaseDate, text, link)
+	var groupID int
+	err := db.pool.QueryRow(ctx, "INSERT INTO groups(group_name) values($1) RETURNING id", group_name).Scan(&groupID)
+	if err != nil {
+		return err
+	}
+	_, err = db.pool.Exec(ctx, "INSERT INTO songs(song_name, releaseDate, text, link, group_id) values($1, TO_TIMESTAMP($2, 'DD.MM.YYYY'), $3, $4, $5)", song_name, releaseDate, text, link, groupID)
 	return err
 }
 
 func (db *PGXDatabase) DeleteQuery(ctx context.Context, group_name string, song_name string) error {
-	_, err := db.pool.Exec(ctx, "DELETE FROM songs WHERE group_name = $1 AND song_name = $2", group_name, song_name)
+	groupID, err := db.SelectGroupIdQuery(ctx, group_name)
+	if err != nil {
+		return err
+	}
+	_, err = db.pool.Exec(ctx, "DELETE FROM songs WHERE group_id = $1 AND song_name = $2", groupID, song_name)
 	return err
 }
 
+func (db *PGXDatabase) SelectGroupIdQuery(ctx context.Context, group_name string) (int, error) {
+	var groupID int
+	err := db.pool.QueryRow(ctx, "SELECT id FROM groups WHERE group_name = $1", group_name).Scan(&groupID)
+	if err != nil {
+		return 0, err
+	}
+	return groupID, nil
+}
+
 func (db *PGXDatabase) SelectDataQuery(ctx context.Context, page int64, items int64, group string, song string, releaseDate string, text string, link string) (AnswerData, error) {
-	query := "SELECT * FROM songs "
+	query := "SELECT g.group_name, s.song_name, TO_CHAR(s.releaseDate, 'DD.MM.YYYY'), s.text, s.link FROM songs s JOIN groups g ON s.group_id = g.id "
 	var answer AnswerData
 	paramindex := 1
 	setClauses := []string{}
 	params := []interface{}{}
 	if group != "" || song != "" || releaseDate != "" || text != "" || link != "" {
 		if group != "" {
-			setClauses = append(setClauses, fmt.Sprintf("group_name = $%d", paramindex))
-			params = append(params, group)
+			var groupID int
+			groupID, err := db.SelectGroupIdQuery(ctx, group)
+			if err != nil {
+				return answer, err
+			}
+			setClauses = append(setClauses, fmt.Sprintf("group_id = $%d", paramindex))
+			params = append(params, groupID)
 			paramindex++
 		}
 		if song != "" {
@@ -97,7 +126,12 @@ func (db *PGXDatabase) SelectDataQuery(ctx context.Context, page int64, items in
 func (db *PGXDatabase) SelectCoupletQuery(ctx context.Context, group string, song string, couplet int64) (AnswerCoupletData, error) {
 	var text string
 	var answer AnswerCoupletData
-	err := db.pool.QueryRow(ctx, "SELECT text FROM songs WHERE group_name = $1 AND song_name = $2", group, song).Scan(&text)
+	var groupID int
+	groupID, err := db.SelectGroupIdQuery(ctx, group)
+	if err != nil {
+		return answer, err
+	}
+	err = db.pool.QueryRow(ctx, "SELECT text FROM songs WHERE group_id = $1 AND song_name = $2", groupID, song).Scan(&text)
 	if err != nil {
 		return answer, err
 	}
@@ -113,9 +147,14 @@ func (db *PGXDatabase) EditQuery(ctx context.Context, group_name string, song_na
 	query := "UPDATE songs SET "
 	paramindex := 3
 	setClauses := []string{}
-	params := []interface{}{group_name, song_name}
+	var groupID int
+	groupID, err := db.SelectGroupIdQuery(ctx, group_name)
+	if err != nil {
+		return err
+	}
+	params := []interface{}{groupID, song_name}
 	if releaseDate != "" {
-		setClauses = append(setClauses, fmt.Sprintf("releaseDate = $%d", paramindex))
+		setClauses = append(setClauses, fmt.Sprintf("releaseDate = TO_TIMESTAMP($%d, 'DD.MM.YYYY')", paramindex))
 		params = append(params, releaseDate)
 		paramindex++
 	}
@@ -133,9 +172,9 @@ func (db *PGXDatabase) EditQuery(ctx context.Context, group_name string, song_na
 		return nil
 	}
 	query += strings.Join(setClauses, ", ")
-	query += fmt.Sprintf(" WHERE group_name = $1 AND song_name = $2")
+	query += fmt.Sprintf(" WHERE group_id = $1 AND song_name = $2")
 	log.Printf("INFO: query for the database=%s\n", query)
 	log.Printf("INFO: query params for the database=%s\n", params)
-	_, err := db.pool.Exec(ctx, query, params...)
+	_, err = db.pool.Exec(ctx, query, params...)
 	return err
 }
